0e5d6efca6757330c17d9faa601a9fd398146e20
wenwen <wenwen79821@gmail.com>
2023-06-23 10:52:46 +0800

    关闭浏览器清除缓存

--- src/pages/summerpc/index.vue
+++ src/pages/summerpc/index.vue
@@ -805,6 +805,12 @@ export default {
     this.getheme();
 
     this.backTop();
+
+    // 关闭浏览器窗口的时候清空浏览器缓存在localStorage的数据
+    window.onbeforeunload = function(e) {
+      var storage = window.localStorage;
+      storage.clear();
+    };
   },
   destroyed() {
     sessionStorage.removeItem("isfirst");
